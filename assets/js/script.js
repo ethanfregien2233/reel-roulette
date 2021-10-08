ccaba05ae970dfee452b81af4b70c38adad4842e
Alex C <alex.christianson.05@gmail.com>
2021-10-08 11:31:34 -0500

    display result from movie search

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,34 @@
-var genreId = 
+var movieTitle = document.querySelector("#movie-title");
+var movieOverview = document.querySelector("#movie-overview");
+var watchInfo = document.querySelector("#where-to-watch");
+var submitBtn = document.querySelector("#submit-button");
+var genreMenu = document.querySelector("#genre-menu");
+var 
 
-var searchMovie = function() {
+var searchMovie = function(event) {
+    event.preventDefault();
+    var genreId = genreMenu.value;
     fetch("https://api.themoviedb.org/3/discover/movie?api_key=e4a0fa8349dde042a07202bf3cbf39c6&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=" + genreId + "&with_watch_monetization_types=rent")
-    .then(function(results) {
-        return results.json()
+    .then(function(data) {
+        return data.json()
     })
-    .then(function(results) {
-        console.log(results);
+    .then(function(data) {
+        console.log(data);
+        var randomMovie = data.results[Math.floor(Math.random() * data.results.length)];
+        console.log(randomMovie);
+        var title = randomMovie.title.split(" ").join("");
+        movieTitle.textContent = randomMovie.title;
+        movieOverview.textContent = randomMovie.overview;
+        console.log(title);
     })
+};
+
+var getWatchInfo = function(title) {
+    // put your fetch request here using "title"
 }
 
-searchMovie();
+var getTrailer = function(title) {
+    // put your fetch request for the trailer here using "title"
+}
+
+submitBtn.addEventListener("click", searchMovie);
