55eebc89b2a7738601d41852f0f0f9ac39f9aada
ethan fregien <ethanfregien@gmail.com>
2021-10-08 13:24:32 -0500

    got youtube api key

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,14 @@ var searchMovie = function() {
 }
 
 searchMovie();
+
+gapi.load("client", loadClient);
+  
+function loadClient() {
+    gapi.client.setApiKey("AIzaSyBvbJ-L8p9Ao3KqmIv5kfPY0NJeHN-1FcA");
+    return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+        .then(function() { console.log("GAPI client loaded for API"); },
+                function(err) { console.error("Error loading GAPI client for API", err); }) };
+
+
+
