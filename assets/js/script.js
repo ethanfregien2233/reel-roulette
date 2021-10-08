58f4d39628e7ccc624e8d28cd819ed98b59c807a
Alex C <alex.christianson.05@gmail.com>
2021-10-08 10:16:13 -0500

    updated api call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
+var genreId = 
+
 var searchMovie = function() {
-    fetch("https://api.themoviedb.org/3/discover/movie?api_key=e4a0fa8349dde042a07202bf3cbf39c6&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=5&with_watch_monetization_types=flatrate")
+    fetch("https://api.themoviedb.org/3/discover/movie?api_key=e4a0fa8349dde042a07202bf3cbf39c6&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=" + genreId + "&with_watch_monetization_types=rent")
     .then(function(results) {
         return results.json()
     })
