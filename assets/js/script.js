cf894b3d1ce645710fff3ad95c5c621b1a44864c
ethan fregien <ethanfregien@gmail.com>
2021-10-11 14:36:18 -0500

    youtube worksgit add .git add .

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,9 @@ var genreMenu = document.querySelector("#genre-menu");
 var trailerContainer = document.querySelector("#trailer-container");
 
 var searchMovie = function(event) {
+    // prevent Browser default refresh
+    event.preventDefault();
+
     var genreId = genreMenu.value;
     fetch("https://api.themoviedb.org/3/discover/movie?api_key=e4a0fa8349dde042a07202bf3cbf39c6&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=" + genreId + "&with_watch_monetization_types=rent")
     .then(function(data) {
@@ -19,26 +22,36 @@ var searchMovie = function(event) {
         movieTitle.textContent = randomMovie.title;
         movieOverview.textContent = randomMovie.overview;
         console.log(title);
+
+        // Now we have Title Data --> 
+        getVideo(title);
     })
+
+    // This code would return before the async fetch method
+
 };
 
 var getWatchInfo = function(title) {
     // put your fetch request here using "title"
 }
 
-function getVideo() {
+function getVideo(query) {
+
+    console.log(`Searching for ${query}`);
+
     $.ajax({
       type: 'GET',
       url: 'https://www.googleapis.com/youtube/v3/search',
       data: {
           key: 'AIzaSyBvbJ-L8p9Ao3KqmIv5kfPY0NJeHN-1FcA',
-          q: "machete trailer",
+          q: `${query}"trailer"`,
           part: 'snippet',
           maxResults: 1,
           type: 'video',
           videoEmbeddable: true,
       },
       success: function(data){
+          console.log(data);
           embedVideo(data)
       },
       error: function(response){
@@ -51,9 +64,11 @@ function getVideo() {
     $('iframe').attr('src', 'https://www.youtube.com/embed/' + data.items[0].id.videoId)
 }
       
-getVideo();
-searchMovie();
+// getVideo();
+// searchMovie();
 
 
 
 submitBtn.addEventListener("click", searchMovie);
+
+//submitBtn.addEventListener("click", getVideo);
