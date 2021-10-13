3834c347cacb3cda3b91aafb3976782a582abce9
ethan fregien <ethanfregien@gmail.com>
2021-10-12 19:19:29 -0500

    s

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,7 @@ var searchMovie = function(event) {
     event.preventDefault();
 
     var genreId = genreMenu.value;
-    fetch("https://api.themoviedb.org/3/discover/movie?api_key=e4a0fa8349dde042a07202bf3cbf39c6&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=" + genreId + "&with_watch_monetization_types=rent")
+    fetch("https://api.themoviedb.org/3/discover/movie?${api_key=e4a0fa8349dde042a07202bf3cbf39c6&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=" + genreId + "&with_watch_monetization_types=rent")
     .then(function(data) {
         return data.json()
     })
